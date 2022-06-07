927431b5c74d654f786680ec569323eeeabbaf11
Max Mohammadi <maxmohammadi@gmail.com>
2022-06-06 23:24:02 -0700

    campaign details

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -2,6 +2,9 @@
 // This  is the Rust style of importing things.
 // Remember we added the dependencies in cargo.toml
 // And from the `solana_program` crate we are including  all the required things.
+
+use borsh::{BorshDeserialize, BorshSerialize};
+
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -18,26 +21,37 @@ use solana_program::{
 // And it is a convention to name it `process_instruction`. 
 // It should take in program_id, accounts, instruction_data as parameters.
 fn process_instruction(
-    // program id is nothing but the id of this program on the solana network.
     program_id: &Pubkey,
-    // When we invoke our program we can 
-    // give meta data of all the account we 
-    // want to work with.
-    // As you can see it is a array of AccountInfo.
-    // We can provide as many as we want.
     accounts: &[AccountInfo],
-    // This is the data we want to process our instruction for.
-    // It is a list of 8 bitunsigned integers(0..255).
     instruction_data: &[u8],
-    
-    // Here we specify the return type.
-    // If you know a little bit of typescript. 
-    // This was of writing types and returns types might we familiar to you.
 ) -> ProgramResult {
-    
-    // And then since we can't return null in Rust we pass `Ok(())` to make it compile
-    // It means the program executed successfully.
-    Ok(())
+
+    if instruction_data.len() == 0 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    if instruction_data[0] == 0 {
+        return create_campaign(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 1 {
+        return withdraw(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 2 {
+        return donate(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    }
+
+    msg!("Didn't find the entrypoint required");
+    Err(ProgramError::InvalidInstructionData);
 }
 
 
@@ -66,4 +80,14 @@ fn donate(
     _instruction_data: &[u8]
 ) -> ProgramResult {
     Ok(())
+}
+
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct CampaignDetails {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub image_link: String,
+    pub amount_donated: u64,
 }
